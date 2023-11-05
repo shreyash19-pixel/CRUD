a8b2b45bcfe71c9786b38458bbf38e4f17842190
shreyash19-pixel <shreyashp734@gmail.com>
2023-11-05 18:24:11 +0530

    first-commit

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { NavWrapper } from '@/styles/NavBar'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <NavWrapper>
+        Hello
+    </NavWrapper>
+  )
+}
+
+export default NavBar
---
+++ src/styles/NavBar/index.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const NavWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    min-height: 50px;
+    align-items: center;
+`
